76b484dbc6aa12b75553717b873fabe07f4a330a
diemus <diemus@users.noreply.github.com>
2020-05-13 15:01:51 +0800

    fix test case

--- error_test.go
+++ error_test.go
@@ -17,4 +17,7 @@ func B() error {
 func TestWrap(t *testing.T) {
 	fmt.Println(B())
 	fmt.Println(Cause(B()))
+	if Cause(B()).Error() != "test" {
+		t.Errorf("cause should equal origin error")
+	}
 }
